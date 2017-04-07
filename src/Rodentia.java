9a80ad898cd650fdb3467727c3796faff2266840
1198189741@qq.com <1198189741@qq.com>
2017-04-07 15:28:21 +0800

    Initial commit

---
+++ src/Rodentia.java
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 2017/4/7.
+ */
+public class Rodentia {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void order() {
+
+        System.out.println("This animal belongs to the Rodentia order.");
+
+    }
+
+}
